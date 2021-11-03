05c8cdcac547ad3fa1069d88b5b81820197a50ea
Fisto <Fisto@DESKTOP-FJR6VHH>
2021-11-03 13:35:49 +0100

    init

---
+++ app/src/main/java/com/example/ps3/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.example.ps3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
